54d2bc350d790cee11ff5db93220b86f875ff335
Unk980929 <102277711+Unk980929@users.noreply.github.com>
2022-07-09 00:06:55 -0400

    Update NexCloudClient.py

--- NexCloudClient.py
+++ NexCloudClient.py
@@ -23,6 +23,7 @@ class NexCloudClient(object):
         self.proxy = None
         if proxy:
             self.proxy = proxy.as_dict_proxy()
+        self.baseheaders = headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'}
 
     def login(self):
         loginurl = self.path + 'index.php/login'
